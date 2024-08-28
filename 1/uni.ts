4a1bffdd40d2d0b249b5e6b3a855971b9d8223f5
ten <ten20031017-sty@ezweb.ne.jp>
2024-08-28 23:20:17 +0900

    全てのパターンに対応

--- 1/uni.ts
+++ 1/uni.ts
@@ -4,7 +4,13 @@ type Success = { done: boolean };
 type Failure = { done: boolean; error: Error };
 
 function printStatus(status: UploadStatus) {
-  if (status.done === false && "progress" in status) {
+  if (status.done) {
+    if ("error" in status) {
+      console.log(`アップロード失敗:${status.error.message}`);
+    } else {
+      console.log(`アップロード成功`);
+    }
+  } else if ("progress" in status) {
     console.log(`アップロード中:${status.progress}%`);
   }
 }
