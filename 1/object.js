a2429859041f7022377434fd11562ffab2e24413
ten <ten20031017-sty@ezweb.ne.jp>
2024-08-28 23:48:14 +0900

    型エイリアスを追加

--- 1/object.js
+++ 1/object.js
@@ -7,3 +7,4 @@ console.log(calculator.sum(1, 1));
 //オブジェクトの型解釈
 var box;
 box = { width: 120, height: 720 };
+var box2 = { width: 1080, height: 720 };
--- 1/object.ts
+++ 1/object.ts
@@ -9,3 +9,7 @@ console.log(calculator.sum(1, 1));
 //オブジェクトの型解釈
 let box: { width: number; height: number };
 box = { width: 120, height: 720 };
+
+//型エイリアス
+type Box = { width: number; height: number };
+let box2: Box = { width: 1080, height: 720 };
