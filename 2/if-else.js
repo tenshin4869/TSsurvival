9438f762e13f9a02b17aae1ca51a702292f7bbb8
ten <ten20031017-sty@ezweb.ne.jp>
2024-08-29 21:39:42 +0900

    三項演算子を追加

---
+++ 2/if-else.js
@@ -0,0 +1,13 @@
+if (value === 0) {
+  //...
+} else if (value === 1) {
+  //...
+} else {
+  //...
+}
+
+//三項演算子項
+//演算の結果は変数に代入できる
+const age = 20;
+const drink = age >= 20 ? "ビール" : "ジュース";
+console.log(drink);
