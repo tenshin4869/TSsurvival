614dff41908208bd652cbf6a3aab46e8bfcde002
ten <ten20031017-sty@ezweb.ne.jp>
2024-08-29 20:45:51 +0900

    テスト解答を記述

--- test/test1.js
+++ test/test1.js
@@ -1,16 +1,13 @@
 //問題：引数に文字列の配列を受け取り、その中身の順番を逆にして返す関数を作成する
 //例：reverseArray(["a", "b", "c"]) => ["c", "b", "a"]
 //実際にデータ型を定義してみよう：変数、引数、戻り値
-
-const reverseArray = (arr: string[]): string[] => {
-  const reverseArray: string[] = [];
-  let index: number = arr.length - 1;
-  while (index >= 0) {
-    reverseArray.push(arr[index]);
-    index--;
-  }
-
-  return reverseArray;
+var reverseArray = function (arr) {
+    var reverseArray = [];
+    var index = arr.length - 1;
+    while (index >= 0) {
+        reverseArray.push(arr[index]);
+        index--;
+    }
+    return reverseArray;
 };
-
 console.log(reverseArray(["a", "b", "c"]));
